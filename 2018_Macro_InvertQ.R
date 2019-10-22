506459b40442fd0e039b5f71525d9a022d8d9b5a
kellyloria <kelo2727@colorado.edu>
2019-10-22 09:24:15 -0600

    Initial document purpose.

---
+++ 2018_Macro_InvertQ.R
@@ -0,0 +1,13 @@
+# Created 2019-10-22 by Kelly Loria #
+
+####
+# QA'QC For:
+#   Sammy Yevak 2018 REU student's Macro-invert data from the GLV
+#     Contains 3 data entities: 
+#           1. A presence absence matrix by site visit
+#           2. Environmental quality per site visit
+#           3. Individual macro invert traits by site visit
+
+# Goals: 
+#   1. Get in parallel form to zooplankton data
+####
