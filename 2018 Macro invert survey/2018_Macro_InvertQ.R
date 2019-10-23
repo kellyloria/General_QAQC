b7382ffe228194fdcb66d12c0a71faa041b029a2
kellyloria <kelo2727@colorado.edu>
2019-10-22 18:49:12 -0600

    Converted file from wide to long. Exported QA'Qced file "MacroInvertAbundance_2018Q.csv"

--- 2018 Macro invert survey/2018_Macro_InvertQ.R
+++ 2018 Macro invert survey/2018_Macro_InvertQ.R
@@ -12,4 +12,26 @@
 #   1. Get in parallel form to zooplankton data
 ####
 
+library(ggplot2)
+library(tidyverse)
+library(reshape2)
+
+#1. Abundance:
+MIAbund <- read_csv("2018 Macro invert survey/MacroInvertAbundance_2018.csv")
+summary(MIAbund)
+
+# Convert wide to long:
+MI_long <- gather(MIAbund, taxa, count, Trichoptera_apataniidae_apatania:Rhynchobdellida_glossiphoniidae, 
+                  factor_key=TRUE)
+
+# Fix date varibles
+MI_long$date <- as.Date.character(MI_long$collection_date, format="%m/%d/%y")
+range(MI_long$date)
+
+# Organize columns
+# select for relevant parameters
+names(MI_long)
+MI_long_exp <- subset(MI_long, select=c(LTER_site, local_site, location, project_site, 
+                                        shore, date, taxa, count))
+summary(MI_long_exp)
 
